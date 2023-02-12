a439817632177864b5e25ab597d776ff45cd4bce
Steve aka ezYakaEagle442 <43554247+ezYakaEagle442@users.noreply.github.com>
2023-02-12 16:31:52 +0100

    Upgrade with latest 2023 Firmware

--- scripts/upgrade-rpi-firmware.sh
+++ scripts/upgrade-rpi-firmware.sh
@@ -9,21 +9,22 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 # Download the Raspberry Pi's Stable firmware
-wget https://github.com/raspberrypi/rpi-eeprom/blob/master/firmware/stable/pieeprom-2020-09-03.bin
+wget https://github.com/raspberrypi/rpi-eeprom/blob/master/firmware/stable/pieeprom-2023-01-11.bin
+# wget https://github.com/raspberrypi/rpi-eeprom/blob/master/firmware/stable/pieeprom-2020-09-03.bin
 # wget https://github.com/raspberrypi/rpi-eeprom/raw/master/firmware/beta/pieeprom-2020-01-17.bin
 
 
 # Pull the boot config and write it to bootconf.txt
-rpi-eeprom-config pieeprom-2020-01-17.bin > bootconf.txt
+rpi-eeprom-config pieeprom-2023-01-11.bin > bootconf.txt
 
 # Replace bootmode 0x1 with 0x21 in the bootconf.txt. This means: try SD first followed by network boot then stop.
 sed -i s/0x1/0x21/g bootconf.txt
 
 # Add the new bootconf.txt to the firmware and save it to a different binary. 
-rpi-eeprom-config --out pieeprom-2020-01-17-netboot.bin --config bootconf.txt pieeprom-2020-01-17.bin
+rpi-eeprom-config --out pieeprom-2023-01-11-netboot.bin --config bootconf.txt pieeprom-2023-01-11.bin
 
 # Update the firmware with the altered .bin file. 
-sudo rpi-eeprom-update -d -f ./pieeprom-2020-01-17-netboot.bin
+sudo rpi-eeprom-update -d -f ./pieeprom-2023-01-11-netboot.bin
 
 # Check the Pi's serial and write down the last 8 characters. You will need this to setup TFTP. 
 cat /proc/cpuinfo
